8836f936a39dc6476149c67b6a00139199cf8a86
marowak <radiant42@yandex.ru>
2023-01-10 22:42:53 +0400

    added sliding window algorithm

---
+++ sliding_window/src/main/java/marowak/example/Main.java
@@ -0,0 +1,30 @@
+package marowak.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(lengthOfLongestSubstring("Hello world!"));
+    }
+
+    // https://itnext.io/sliding-window-algorithm-technique-6001d5fbe8b3#:~:text=The%20Sliding%20window%20is%20a,complexity%20to%20O(n).
+    public static int lengthOfLongestSubstring(String s) {
+        int maxCount = 0;
+        // start for window
+        int start = 0;
+        // next index for current character
+        int[] nextIndex = new int[128];
+
+        for(int end = 0; end < s.length(); end++) {
+            char val = s.charAt(end);
+            // if character repeats, get next index
+            start = Math.max(start, nextIndex[val]);
+
+            int currCount = end - start + 1;
+            maxCount = Math.max(currCount, maxCount);
+
+            // update where we met current character
+            nextIndex[val] = end + 1;
+        }
+
+        return maxCount;
+    }
+}
