7ceed72d2c94261ec0a331e4314f9fc3d09563ac
marowak <radiant42@yandex.ru>
2023-01-30 14:39:46 +0400

    added task scheduler algorithm

--- greedy_algorithm/src/main/java/org/example/Main.java
+++ greedy_algorithm/src/main/java/org/example/Main.java
@@ -5,19 +5,23 @@ public class Main {
         String[] arr = new String[]{"aa", "ab", "ba"};
         int result = longestPalindrome(arr);
         System.out.println("result: " + result);
+
+
+        char[] tasks = new char[]{'A', 'A', 'A', 'B', 'B', 'B', 'C', 'C', 'C'};
+        System.out.println("task scheduler result: " + taskScheduler(tasks, 2));
     }
 
     private static int longestPalindrome(String[] words) {
         int[][] counter = new int[26][26];
 
         int result = 0;
-        for(String word : words) {
+        for (String word : words) {
             int f = word.charAt(0) - 'a';
             int s = word.charAt(1) - 'a';
 
             // если для текущего слова есть палиндром, увеличивае результат (2 слова по 2 буквы)
             // и уменьшаем счетчик
-            if(counter[f][s] > 0) {
+            if (counter[f][s] > 0) {
                 result += 4;
                 counter[f][s]--;
             } else {
@@ -35,5 +39,46 @@ public class Main {
         }
 
         return result;
+    }
+
+
+    /**
+     * 1 задача выполняется 1 единицу времени
+     *
+     * @param tasks список задач. Один символ A-Z
+     * @param n     интервал простоя между одинаковыми задачами
+     * @return общее время выполенения всех задач
+     */
+    public static int taskScheduler(char[] tasks, int n) {
+        int[] counter = new int[26];
+
+        // максимальное количество задач одного типа
+        int maxCount = 0;
+
+        // количество задач с максимальным значением
+        int tasksWithMaxCount = 0;
+        for (char task : tasks) {
+            int idx = task - 'A';
+            counter[idx]++;
+
+            if (maxCount == counter[idx]) {
+                tasksWithMaxCount++;
+            } else if (maxCount < counter[idx]) {
+                maxCount = counter[idx];
+                tasksWithMaxCount = 1;
+            }
+        }
+
+        // количество промежутков между максимально повторяемой задачей
+        int partCount = maxCount - 1;
+        int partLength = n - (tasksWithMaxCount - 1);
+        int emptySlots = partCount * partLength;
+        int availableTasks = tasks.length - maxCount * tasksWithMaxCount;
+
+        // если пустых слотов между задачами больше, чем дотсупно задач,
+        // то эти слоты нужно учитывать в конечном ответе
+        int idles = Math.max(0, emptySlots - availableTasks);
+
+        return tasks.length + idles;
     }
 }
